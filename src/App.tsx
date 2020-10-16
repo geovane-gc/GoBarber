49a998fb9f277771383ac745309d9b66aea937a8
Geovane <geovanegallotticazal@hotmail.com>
2020-10-16 12:32:39 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
